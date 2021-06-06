6f9ee74b5f03fc69cea548ac1649187a05e3c14d
vidal1101 <rodrigovidal278@gmail.com>
2021-06-06 16:16:26 -0600

    routes, conexion a mysql con pewee ORM

---
+++ APY/database.py
@@ -0,0 +1,18 @@
+from peewee  import *
+
+Database = MySQLDatabase('peliculas' ,
+                        user='root',
+                        password='Runo1101',
+                        host='localhost')
+
+
+class User(Model):
+    username = CharField(max_length=50)
+    email = CharField(max_length=50)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        database =Database
+        table_name='Usuarios'
